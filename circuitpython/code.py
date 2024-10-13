21a034359f42a07ca0fb28f3a91b424a2ebdeabf
Sheepy3 <sheepy680@gmail.com>
2024-10-13 18:51:03 -0400

    new code yipee

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -16,36 +16,52 @@ channel_a = AnalogIn(hx711, HX711.CHAN_A_GAIN_128)
 tare_value = 33920  # This is the midpoint for a 24-bit ADC (2^23)
 known_weight_g = 19.316
 known_weight_reading = 73400  # This is an example value, you need to measure this
+MAX_RAW_VALUE = 8388607
 scale_factor = (known_weight_reading-tare_value)/known_weight_g
 
 data_points = []
 current_output = 0
 dirty_points = []
-def moving_average_filter(new_value, window_size=10):
+
+def moving_average_filter(new_value, window_size=5):
     global data_points
     global dirty_points
-    if current_output/new_value > 0.05:
+    if new_value == 0 or new_value == MAX_RAW_VALUE:
+       #print("ADC issue detected.")
+       return 0
+    
+    converted_new_value = get_weight_g(new_value)
+    valuediff = (abs(current_output-converted_new_value)/abs((current_output+converted_new_value)/2))*100
+    #print(f"% Diff: {valuediff}")
+    if valuediff > 199:
         if len(dirty_points) < 2:
-            print(f"DIRTY READING. DISCARDING.")
+            #print(f"DIRTY READING. DISCARDING.")
             dirty_points.append(new_value)
-            return current_output
+            return 0            
         else:
-            print(f"too. much. dirt.")
+            #print(f"too. much. dirt.")
             dirty_points = []
             data_points = []
-    
-    data_points.append(new_value)
-    
+     
+    data_points.append(new_value)    
     if len(data_points) > window_size:
         data_points = data_points[-window_size:]
-    
+  
     return sum(data_points) / len(data_points)
+
+
+
 
 def get_weight_g(reading):
     net_value = reading - tare_value
-    return net_value / scale_factor
+    weight = net_value / scale_factor 
+    #print(f"Raw: {reading}, Net: {net_value}, Weight: {weight}g")
+    return weight
 
 while True:
-    current_output = get_weight_g(moving_average_filter(channel_a.value))
-    print(f"Reading: {current_output}g")
+    get_new_value = moving_average_filter(channel_a.value)
+    if get_new_value != 0:
+        current_output = get_weight_g(get_new_value)
+    #print(data_points)
+    print(f"Reading: {round(current_output,3)}g")
     #time.sleep(0.01)
--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
 # Dead Simple Thrust Stand
 
-the current design is extremely rudimentary and can be described with this schematc:![1728850611883](image/readme/1728850611883.png)
+I needed a thrust stand in order to get better data when designing RC aircraft. The bom for this project is as follows:
+
+| Component             | Source | Quantity | Notes                                                                                       |
+| --------------------- | ------ | -------- | ------------------------------------------------------------------------------------------- |
+| 2KG load cell + HX711 |        | 1        | Larger Load cell recommended for more powerful motors.                                      |
+| M4x20                 |        | 2        | Set of varied screw sizes.                                                                  |
+| M4x10                 |        | 2        | You can also use M4x20 here.                                                                |
+| m2x10                 |        | 2        | Used to mount motor to thrust tower.                                                        |
+| 3D Printed Parts      | Self   | 2        | STEP files available here and on printables. PETG recommended as PLA will creep under load. |
+
+the current design is extremely rudimentary and can be described with this schematc:![1728850611883](image/readme/1728850611883.png)the current design does not use a pcb because of how rudimentary it is. I have included the kicad files regardless for posterity. Future versions may take advantage of it. 
 
 # Code
 
-I hate programming so I wrote the firmware for the thing in circuitpython. that can be found here
+I hate programming so I wrote the firmware for the thing in circuitpython. that can be found here. It only requires the HX711 library which can be found here. The code includes some rudimentary filtering as well.
